1a1f08623dbc2fe3ca095f97946fc9ab6c3337c6
lotaplota <kristhian.lucas@gmail.com>
2026-09-12 13:13:11 -0300

    feat: Adiciona funções e propriedades

--- src/Tamago.java
+++ src/Tamago.java
@@ -1,12 +1,17 @@
 public class Tamago {
     private double pv;
     private double fome;
+    private double saude; // Tomar banho pra evitar doencas(?)
     private final String nome;
 
     public Tamago(String nome) {
         this.nome = nome;
         this.pv = 100;
         this.fome = 0;
+    }
+
+    public void Explorar() {
+        // Explorar gasta tempo e saúde, mas tem chance de encontrar um talher ou remédio
     }
 
     @Override
